fc394381800fbb02ca72f3b5c6b27a00c609c4e1
Fixel <zielnyfranek@gmail.com>
2024-08-17 12:50:42 +0200

    Added SilentWolf api for leaderboard

--- globals/globals.gd
+++ globals/globals.gd
@@ -11,3 +11,15 @@ var points: int = 0:
 		print(value)
 	get:
 		return points
+
+var api_key: String
+
+func _ready() -> void:
+	var file = FileAccess.open('res://api_key.env', FileAccess.READ)
+	api_key = file.get_as_text()
+	
+	SilentWolf.configure({
+		"api_key": self.api_key,
+		"game_id": "Builttoscale12",
+		"log_level": 1
+ 	})
