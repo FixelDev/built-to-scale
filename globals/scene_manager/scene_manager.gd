030deb2d9b04432dcbb2d6171afd29e5fbb1f2ad
Fixel <zielnyfranek@gmail.com>
2024-08-18 21:10:20 +0200

    Added transitions

--- globals/scene_manager/scene_manager.gd
+++ globals/scene_manager/scene_manager.gd
@@ -4,6 +4,19 @@ extends Node2D
 @export var game_scene: PackedScene
 @export var game_over_scene: PackedScene
 
+@onready var transition_panel = %TransitionPanel
+
 
 func load_scene(scene: PackedScene) -> void:
+	
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_method(set_shader_value, 1.0, 0.0, 0.7)
+	
+	await tween.finished
 	get_tree().change_scene_to_packed(scene)
+	
+	tween = get_tree().create_tween()
+	tween.tween_method(set_shader_value, 0.0, 1.0, 0.7)
+
+func set_shader_value(value: float) -> void:
+	transition_panel.material.set_shader_parameter("circle_size", value)
